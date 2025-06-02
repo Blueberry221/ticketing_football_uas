e975196865c7528f8023b1e087ecd45de9cd1646
AM4517UMOR4NG <aekmohop@gmail.com>
2025-06-02 21:55:56 +0700

    Penambahan Router

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AreasController;
+use App\Http\Controllers\TeamsController;
+use App\Http\Controllers\SeatsController;
+use App\Http\Controllers\MatchesController;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\TicketsController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::resource('areas', AreasController::class);
+Route::resource('teams', TeamsController::class);
+Route::resource('seats', SeatsController::class);
+Route::resource('matches', MatchesController::class);
+Route::resource('users', UsersController::class);
+Route::resource('tickets', TicketsController::class);
