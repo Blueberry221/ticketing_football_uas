ab817a7ace8134090500bba5c6b2975819d355e0
AM4517UMOR4NG <aekmohop@gmail.com>
2025-06-02 21:43:55 +0700

    Penambahan Router

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AreasController;
+use App\Http\Controllers\TeamsController;
+use App\Http\Controllers\SeatsController;
+use App\Http\Controllers\MatchesController;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\TicketsController;
 
+
+// Rute Halaman Utama
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+// Resource Routes - Kompatibel 100% dengan Controller Anda
+Route::resource('areas', AreasController::class);
+Route::resource('teams', TeamsController::class);
+Route::resource('seats', SeatsController::class);
+Route::resource('matches', MatchesController::class);
+Route::resource('users', UsersController::class);
+Route::resource('tickets', TicketsController::class);
